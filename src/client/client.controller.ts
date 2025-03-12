724b9fbc02d11d4df1d8b91dbd399cc80b301fb4
CamiloA-17 <camiangi1517@gmail.com>
2025-03-11 22:22:23 -0500

    Clients endopoints created

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ClientService } from './client.service';
+import { Client } from './client.entity';
 
-@Controller('client')
-export class ClientController {}
+@Controller('clients')
+export class ClientController {
+
+    constructor(private clientsService: ClientService) {}
+
+    @Post()
+    async create(@Body() client: Client) {
+        return this.clientsService.create(client);
+    }
+
+    @Get()
+    async findAll() {
+        return this.clientsService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        return this.clientsService.findOne(id);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: string) {
+        return this.clientsService.remove(id);
+    }
+
+}
